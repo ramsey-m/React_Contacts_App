9665de3ade23d2d063eaed121da209e57e48a1e7
ramsey-m <53435422+ramsey-m@users.noreply.github.com>
2021-09-23 14:27:33 -0500

    added new compoents

---
+++ components/Friends.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Friends() {
+  return (
+    <div>
+      <p>friendtest</p>
+    </div>
+  );
+}
+
+export default Friends;
---
+++ components/MyCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyCard() {
+  return (
+    <div>
+      <p>MyCard</p>
+    </div>
+  );
+}
+
+export default MyCard;
