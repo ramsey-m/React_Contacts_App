48d5606ce7e6dd69f85fe6b5c9dadbde5a119f99
ramsey-m <53435422+ramsey-m@users.noreply.github.com>
2021-09-24 05:17:51 -0500

    friday refresh

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <h2>Cohort 12 Contact Info</h2>
+          <h2>Cohort 12 Contact Information</h2>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <ul className="navbar-nav mr-auto">
               <li>
